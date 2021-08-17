f4fa4bd8f079b175b7405f9f1f793233faa0e7e0
laszej <88169478+laszej@users.noreply.github.com>
2021-08-17 21:49:49 +0200

    Add files via upload

---
+++ liczenie.js
@@ -0,0 +1,15 @@
+function sprawdz()
+{
+let liczba = document.getElementById("pole").value;
+let uchwyt = document.getElementById("wynik");
+
+if (liczba==="" || liczba === " ") uchwyt.innerHTML="wprowadź liczbę";
+else if (liczba>0) uchwyt.innerHTML="dodatnia";
+else if (liczba<0) uchwyt.innerHTML="ujemna";
+else if (liczba==0) uchwyt.innerHTML="zero";
+
+else document.uchwyt.innerHTML="to nie jest liczba";
+
+} 
+
+
---
+++ script.js
@@ -0,0 +1,5 @@
+let a = 2;
+let b = 4;
+function mnozenie()
+{a*b};
+console.log ("mnozenie");
